8c2be264948a0e0064430294833af13665c7deef
Deligor6321 <deligor6321@gmail.com>
2023-11-05 19:00:53 +0200

    lib: Add mask support to enum_flags

--- include/dlgr/enum_flags.h
+++ include/dlgr/enum_flags.h
@@ -8,22 +8,82 @@
 
 namespace dlgr {
 
-template <class E, std::make_unsigned_t<std::underlying_type_t<E>> M =
-                       std::numeric_limits<std::make_unsigned_t<std::underlying_type_t<E>>>::max()>
-  requires std::is_enum_v<E>
-class enum_flags {
+// == Implementation details declarations and utils
+
+namespace detail {
+
+template <class EnumType>
+using enum_flags_data_t = std::make_unsigned_t<std::underlying_type_t<EnumType>>;
+
+template <class EnumType, enum_flags_data_t<EnumType> EffectiveMask>
+  requires std::is_enum_v<EnumType>
+class enum_flags_impl;
+
+template <class EnumType, EnumType... EnumValues>
+struct make_enum_flags_mask {
+  using enum_type = EnumType;
+  using enum_flags_data_type = detail::enum_flags_data_t<enum_type>;
+
+  constexpr static auto value =
+      (enum_flags_data_type(0) | ... | std::bit_cast<enum_flags_data_type>(EnumValues));
+};
+
+template <class EnumType, EnumType... EnumValues>
+constexpr inline auto make_enum_flags_mask_v = make_enum_flags_mask<EnumType, EnumValues...>::value;
+
+}  // namespace detail
+
+// == Template declaration
+
+template <class Enum, class Mask>
+class enum_flags;
+
+// == Mask specification
+
+struct enum_flags_mask_unspecified_t {};
+
+template <std::unsigned_integral MaskType, MaskType MaskValue>
+struct enum_flags_mask_spec_t : std::integral_constant<MaskType, MaskValue> {};
+
+template <class EnumType>
+  requires std::is_enum_v<EnumType>
+struct make_enum_flags_mask_spec {
+  template <EnumType... EnumValues>
+  struct from_enums {
+   private:
+    using mask_maker_type = detail::make_enum_flags_mask<EnumType, EnumValues...>;
+
+   public:
+    using type = enum_flags_mask_spec_t<typename mask_maker_type::enum_flags_data_type,
+                                        mask_maker_type::value>;
+  };
+
+  template <EnumType... EnumValues>
+  using from_enums_t = typename from_enums<EnumValues...>::type;
+};
+
+// == Template specialization for unspecified mask
+
+template <class EnumType>
+  requires std::is_enum_v<EnumType>
+class enum_flags<EnumType, enum_flags_mask_unspecified_t> {
+  using enum_flags_impl_type =
+      detail::enum_flags_impl<EnumType,
+                              std::numeric_limits<detail::enum_flags_data_t<EnumType>>::max()>;
+
  public:
   // -- Member types
 
-  using flag_type = E;
-  using underlying_data_type = std::make_unsigned_t<std::underlying_type_t<flag_type>>;
+  using flag_type = typename enum_flags_impl_type::flag_type;
+  using underlying_data_type = typename enum_flags_impl_type::underlying_data_type;
+  using mask_spec_type = enum_flags_mask_unspecified_t;
 
   // -- Constructors
 
   [[nodiscard]] constexpr enum_flags() noexcept = default;
 
   [[nodiscard]] constexpr enum_flags(flag_type flag) noexcept  // NOLINT: Allow non-explicit ctor
-      : enum_flags(std::bit_cast<underlying_data_type>(flag)) {}
+      : flags_(flag) {}
 
   // -- Comparison
 
@@ -34,7 +94,7 @@ class enum_flags {
   [[nodiscard]] constexpr explicit operator bool() const noexcept { return has_any(); }
 
   [[nodiscard]] constexpr explicit operator enum_flags::underlying_data_type() const noexcept {
-    return flags_data_;
+    return static_cast<underlying_data_type>(flags_);
   }
 
   // -- Access
@@ -43,53 +103,187 @@ class enum_flags {
     return test(flag);
   }
 
-  [[nodiscard]] constexpr auto test(enum_flags flags) const noexcept -> bool {
-    return (*this & flags) == flags;
+  [[nodiscard]] constexpr auto test(flag_type flag) const noexcept -> bool {
+    return flags_.test(flag);
   }
 
-  [[nodiscard]] constexpr auto has_all() const noexcept -> bool {
-    return (*this == enum_flags::all());
+  [[nodiscard]] constexpr auto test(enum_flags flags) const noexcept -> bool {
+    return flags_.test(flags.flags_);
   }
 
   [[nodiscard]] constexpr auto has_any() const noexcept -> bool { return !has_none(); }
 
-  [[nodiscard]] constexpr auto has_none() const noexcept -> bool {
-    return (*this == enum_flags::none());
-  }
+  [[nodiscard]] constexpr auto has_none() const noexcept -> bool { return flags_.has_none(); }
 
   // -- Modification
 
   constexpr auto operator|=(enum_flags other) noexcept -> enum_flags& {
-    flags_data_ |= other.flags_data_;
+    flags_ |= other.flags_;
     return *this;
   }
 
   constexpr auto operator&=(enum_flags other) noexcept -> enum_flags& {
-    flags_data_ &= other.flags_data_;
+    flags_ &= other.flags_;
     return *this;
   }
 
   constexpr auto operator^=(enum_flags other) noexcept -> enum_flags& {
-    flags_data_ ^= other.flags_data_;
+    flags_ ^= other.flags_;
     return *this;
   }
 
-  constexpr auto set(flag_type flag) noexcept -> enum_flags& { return (*this |= enum_flags(flag)); }
-
-  constexpr auto reset(flag_type flag) noexcept -> enum_flags& {
-    return (*this &= enum_flags(flag).flip_all());
+  constexpr auto set(enum_flags flags) noexcept -> enum_flags& {
+    flags_.set(flags.flags_);
+    return *this;
   }
 
-  constexpr auto flip(flag_type flag) noexcept -> enum_flags& {
-    return test(flag) ? reset(flag) : set(flag);
+  constexpr auto reset(enum_flags flags) noexcept -> enum_flags& {
+    flags_.reset(flags.flags_);
+    return *this;
   }
 
-  constexpr auto set_all() noexcept -> enum_flags& { return (*this = enum_flags::all()); }
+  constexpr auto flip(enum_flags flags) noexcept -> enum_flags& {
+    flags_.flip(flags.flags_);
+    return *this;
+  }
 
-  constexpr auto reset_all() noexcept -> enum_flags& { return (*this = enum_flags::none()); }
+  constexpr auto reset_all() noexcept -> enum_flags& {
+    flags_.reset_all();
+    return *this;
+  }
+
+  // -- Non-member functions
+
+  [[nodiscard]] friend constexpr auto operator|(enum_flags lhs, enum_flags rhs) noexcept
+      -> enum_flags {
+    return (lhs |= rhs);
+  }
+
+  [[nodiscard]] friend constexpr auto operator&(enum_flags lhs, enum_flags rhs) noexcept
+      -> enum_flags {
+    return (lhs &= rhs);
+  }
+
+  [[nodiscard]] friend constexpr auto operator^(enum_flags lhs, enum_flags rhs) noexcept
+      -> enum_flags {
+    return (lhs ^= rhs);
+  }
+
+  // -- Static functions
+
+  [[nodiscard]] static constexpr auto none() noexcept -> enum_flags {
+    auto flags = enum_flags();
+    flags.flags_ = enum_flags_impl_type::none();
+    return flags;
+  }
+
+ private:
+  enum_flags_impl_type flags_;
+};
+
+// == Template specialization for specified mask
+
+template <class EnumType, detail::enum_flags_data_t<EnumType> Mask>
+  requires std::is_enum_v<EnumType>
+class enum_flags<EnumType, enum_flags_mask_spec_t<decltype(Mask), Mask>> {
+  using enum_flags_impl_type = detail::enum_flags_impl<EnumType, Mask>;
+  static_assert(enum_flags_mask_spec_t<decltype(Mask), Mask>::value ==
+                enum_flags_impl_type::effective_mask);
+
+ public:
+  // -- Member types
+
+  using flag_type = typename enum_flags_impl_type::flag_type;
+  using underlying_data_type = typename enum_flags_impl_type::underlying_data_type;
+  using mask_spec_type = enum_flags_mask_spec_t<decltype(Mask), Mask>;
+
+  // -- Static members
+
+  constexpr static auto mask = mask_spec_type::value;
+
+  // -- Constructors
+
+  [[nodiscard]] constexpr enum_flags() noexcept = default;
+
+  [[nodiscard]] constexpr enum_flags(flag_type flag) noexcept  // NOLINT: Allow non-explicit ctor
+      : flags_(flag) {}
+
+  // -- Comparison
+
+  [[nodiscard]] constexpr auto operator==(const enum_flags&) const noexcept -> bool = default;
+
+  // -- Conversion
+
+  [[nodiscard]] constexpr explicit operator bool() const noexcept { return has_any(); }
+
+  [[nodiscard]] constexpr explicit operator enum_flags::underlying_data_type() const noexcept {
+    return static_cast<underlying_data_type>(flags_);
+  }
+
+  // -- Access
+
+  [[nodiscard]] constexpr auto operator&(flag_type flag) const noexcept -> bool {
+    return test(flag);
+  }
+
+  [[nodiscard]] constexpr auto test(flag_type flag) const noexcept -> bool {
+    return flags_.test(flag);
+  }
+
+  [[nodiscard]] constexpr auto test(enum_flags flags) const noexcept -> bool {
+    return flags_.test(flags.flags_);
+  }
+
+  [[nodiscard]] constexpr auto has_all() const noexcept -> bool { return flags_.has_all(); }
+
+  [[nodiscard]] constexpr auto has_any() const noexcept -> bool { return !has_none(); }
+
+  [[nodiscard]] constexpr auto has_none() const noexcept -> bool { return flags_.has_none(); }
+
+  // -- Modification
+
+  constexpr auto operator|=(enum_flags other) noexcept -> enum_flags& {
+    flags_ |= other.flags_;
+    return *this;
+  }
+
+  constexpr auto operator&=(enum_flags other) noexcept -> enum_flags& {
+    flags_ &= other.flags_;
+    return *this;
+  }
+
+  constexpr auto operator^=(enum_flags other) noexcept -> enum_flags& {
+    flags_ ^= other.flags_;
+    return *this;
+  }
+
+  constexpr auto set(enum_flags flags) noexcept -> enum_flags& {
+    flags_.set(flags.flags_);
+    return *this;
+  }
+
+  constexpr auto reset(enum_flags flags) noexcept -> enum_flags& {
+    flags_.reset(flags.flags_);
+    return *this;
+  }
+
+  constexpr auto flip(enum_flags flags) noexcept -> enum_flags& {
+    flags_.flip(flags.flags_);
+    return *this;
+  }
+
+  constexpr auto set_all() noexcept -> enum_flags& {
+    flags_.set_all();
+    return *this;
+  }
+
+  constexpr auto reset_all() noexcept -> enum_flags& {
+    flags_.reset_all();
+    return *this;
+  }
 
   constexpr auto flip_all() noexcept -> enum_flags& {
-    flags_data_ = (~flags_data_ & M);
+    flags_.flip_all();
     return *this;
   }
 
@@ -116,15 +310,144 @@ class enum_flags {
 
   // -- Static functions
 
-  [[nodiscard]] static constexpr auto all() noexcept -> enum_flags { return enum_flags(M); }
+  [[nodiscard]] static constexpr auto all() noexcept -> enum_flags {
+    auto flags = enum_flags();
+    flags.flags_ = enum_flags_impl_type::all();
+    return flags;
+  }
 
-  [[nodiscard]] static constexpr auto none() noexcept -> enum_flags { return enum_flags(0); }
+  [[nodiscard]] static constexpr auto none() noexcept -> enum_flags {
+    auto flags = enum_flags();
+    flags.flags_ = enum_flags_impl_type::none();
+    return flags;
+  }
 
  private:
-  [[nodiscard]] constexpr explicit enum_flags(underlying_data_type flags_data) noexcept
-      : flags_data_(flags_data) {}
+  enum_flags_impl_type flags_;
+};
+
+// == Deduction guides
+template <class EnumType>
+enum_flags(EnumType) -> enum_flags<EnumType, enum_flags_mask_unspecified_t>;
+
+template <class EnumType, detail::enum_flags_data_t<EnumType> Mask>
+enum_flags(EnumType, enum_flags_mask_spec_t<detail::enum_flags_data_t<EnumType>, Mask>)
+    -> enum_flags<EnumType, enum_flags_mask_spec_t<detail::enum_flags_data_t<EnumType>, Mask>>;
+
+// == Implementation details
+
+namespace detail {
+template <class EnumType, enum_flags_data_t<EnumType> EffectiveMask>
+  requires std::is_enum_v<EnumType>
+class enum_flags_impl {
+ public:
+  // -- Member types
+
+  using flag_type = EnumType;
+  using underlying_data_type = enum_flags_data_t<flag_type>;
+
+  // -- Static members
+
+  constexpr static underlying_data_type effective_mask = EffectiveMask;
+
+  // -- Constructors
+
+  [[nodiscard]] constexpr enum_flags_impl() noexcept = default;
+
+  [[nodiscard]] constexpr explicit enum_flags_impl(flag_type flag) noexcept
+      : enum_flags_impl(std::bit_cast<underlying_data_type>(flag)) {}
+
+  // -- Comparison
+
+  [[nodiscard]] constexpr auto operator==(const enum_flags_impl&) const noexcept -> bool = default;
+
+  // -- Conversion
+
+  [[nodiscard]] constexpr explicit operator enum_flags_impl::underlying_data_type() const noexcept {
+    return flags_data_;
+  }
+
+  // -- Access
+  [[nodiscard]] constexpr auto test(flag_type flag) const noexcept -> bool {
+    if (!enum_flags_impl::can_represent(flag)) {
+      return false;
+    }
+    return test(enum_flags_impl(flag));
+  }
+
+  [[nodiscard]] constexpr auto test(enum_flags_impl flags) const noexcept -> bool {
+    return (flags_data_ & flags.flags_data_) == flags.flags_data_;
+  }
+
+  [[nodiscard]] constexpr auto has_all() const noexcept -> bool {
+    return (*this == enum_flags_impl::all());
+  }
+
+  [[nodiscard]] constexpr auto has_none() const noexcept -> bool {
+    return (*this == enum_flags_impl::none());
+  }
+
+  // -- Modification
+
+  constexpr auto operator|=(enum_flags_impl other) noexcept -> enum_flags_impl& {
+    flags_data_ |= other.flags_data_;
+    return *this;
+  }
+
+  constexpr auto operator&=(enum_flags_impl other) noexcept -> enum_flags_impl& {
+    flags_data_ &= other.flags_data_;
+    return *this;
+  }
+
+  constexpr auto operator^=(enum_flags_impl other) noexcept -> enum_flags_impl& {
+    flags_data_ ^= other.flags_data_;
+    return *this;
+  }
+
+  constexpr auto set(enum_flags_impl flags) noexcept -> enum_flags_impl& {
+    return (*this |= flags);
+  }
+
+  constexpr auto reset(enum_flags_impl flags) noexcept -> enum_flags_impl& {
+    return (*this &= flags.flip_all());
+  }
+
+  constexpr auto flip(enum_flags_impl flags) noexcept -> enum_flags_impl& {
+    return test(flags) ? reset(flags) : set(flags);
+  }
+
+  constexpr auto set_all() noexcept -> enum_flags_impl& { return (*this = enum_flags_impl::all()); }
+
+  constexpr auto reset_all() noexcept -> enum_flags_impl& {
+    return (*this = enum_flags_impl::none());
+  }
+
+  constexpr auto flip_all() noexcept -> enum_flags_impl& {
+    flags_data_ = (~flags_data_ & enum_flags_impl::effective_mask);
+    return *this;
+  }
+
+  // -- Static functions
+
+  [[nodiscard]] static constexpr auto all() noexcept -> enum_flags_impl {
+    return enum_flags_impl(effective_mask);
+  }
+
+  [[nodiscard]] static constexpr auto none() noexcept -> enum_flags_impl {
+    return enum_flags_impl(0);
+  }
+
+  static constexpr auto can_represent(flag_type flag) noexcept -> bool {
+    return enum_flags_impl(flag).flags_data_ == std::bit_cast<underlying_data_type>(flag);
+  }
+
+ private:
+  [[nodiscard]] constexpr explicit enum_flags_impl(underlying_data_type flags_data) noexcept
+      : flags_data_(flags_data & enum_flags_impl::effective_mask) {}
 
   underlying_data_type flags_data_ = 0;
 };
+
+}  // namespace detail
 
 }  // namespace dlgr
--- tests/src/test_enum_flags.cc
+++ tests/src/test_enum_flags.cc
@@ -11,6 +11,8 @@
 namespace {
 
 using dlgr::enum_flags;
+using dlgr::enum_flags_mask_unspecified_t;
+using dlgr::make_enum_flags_mask_spec;
 
 constexpr auto to_bitset(auto flags) {
   using underlying_data_t = typename decltype(flags)::underlying_data_type;
@@ -24,13 +26,86 @@ enum class my_flag : std::uint8_t {
 
   first_and_second = first | second,
 };
-constexpr std::uint8_t my_flag_mask = 0b00000111;
 
 }  // namespace
 
 // NOLINTBEGIN
-TEST_CASE("test_enum_flags", "test_my_flag") {
-  using test_flags_t = enum_flags<my_flag, my_flag_mask>;
+TEST_CASE("enum_flags_wo_mask") {
+  using test_flags_t = enum_flags<my_flag, enum_flags_mask_unspecified_t>;
+
+  STATIC_CHECK(std::is_trivially_copyable_v<test_flags_t>);
+  STATIC_CHECK(std::is_standard_layout_v<test_flags_t>);
+
+  STATIC_CHECK(to_bitset(test_flags_t::none()) == 0b00000000);
+  STATIC_CHECK(test_flags_t::none().has_none());
+  STATIC_CHECK(!test_flags_t::none().has_any());
+
+  test_flags_t flags1;
+  CHECK(to_bitset(flags1) == 0b00000000);
+  CHECK(!flags1.has_any());
+  CHECK(!static_cast<bool>(flags1));
+  CHECK(flags1.has_none());
+
+  test_flags_t flags2 = my_flag::third;
+  CHECK(to_bitset(flags2) == 0b00000100);
+  CHECK(flags2.has_any());
+  CHECK(static_cast<bool>(flags2));
+  CHECK(!flags2.has_none());
+  CHECK(flags2 & my_flag::third);
+
+  auto flags3 = test_flags_t() | my_flag::first | my_flag::second;
+  CHECK(to_bitset(flags3) == 0b00000011);
+  CHECK(flags3 & my_flag::first);
+  CHECK(flags3.test(my_flag::second));
+  CHECK(flags3 & my_flag::first_and_second);
+  CHECK(!flags3.test(my_flag::third));
+
+  flags3 |= flags2;
+  CHECK(to_bitset(flags3) == 0b00000111);
+  CHECK(flags3 & my_flag::third);
+
+  flags3 ^= my_flag::second;
+  CHECK(to_bitset(flags3) == 0b00000101);
+  CHECK(flags3.test(my_flag::first));
+  CHECK(!flags3.test(my_flag::second));
+  CHECK(!flags3.test(my_flag::first_and_second));
+
+  flags1 = flags3 & flags2;
+  CHECK(to_bitset(flags1) == 0b00000100);
+
+  flags1 = flags3 ^ flags2;
+  CHECK(to_bitset(flags1) == 0b00000001);
+  CHECK(flags1 == my_flag::first);
+
+  flags1 = flags2 | flags1;
+  CHECK(to_bitset(flags1) == 0b00000101);
+  CHECK(flags1 == flags3);
+
+  flags1 &= my_flag::third;
+  CHECK(to_bitset(flags1) == 0b00000100);
+
+  flags1.set(my_flag::second).reset(my_flag::third);
+  CHECK(to_bitset(flags1) == 0b00000010);
+
+  flags3.flip(my_flag::first_and_second);
+  CHECK(to_bitset(flags3) == 0b00000111);
+  flags3.flip(my_flag::first_and_second);
+  CHECK(to_bitset(flags3) == 0b00000100);
+
+  flags2.reset_all().flip(my_flag::first);
+  CHECK(to_bitset(flags2) == 0b00000001);
+  CHECK(flags2.test(my_flag::first));
+
+  flags2.set(my_flag::third).set(my_flag::second).flip(my_flag::first_and_second);
+  CHECK(to_bitset(flags2) == 0b00000100);
+  CHECK(flags2.test(my_flag::third));
+}
+
+TEST_CASE("enum_flags_with_mask") {
+  using test_flags_t =
+      enum_flags<my_flag,
+                 make_enum_flags_mask_spec<my_flag>::from_enums_t<
+                     my_flag::first, my_flag::second, my_flag::third, my_flag::first_and_second>>;
 
   STATIC_CHECK(std::is_trivially_copyable_v<test_flags_t>);
   STATIC_CHECK(std::is_standard_layout_v<test_flags_t>);
@@ -105,6 +180,100 @@ TEST_CASE("test_enum_flags", "test_my_flag") {
 
   flags2.flip_all();
   CHECK(to_bitset(flags2) == 0b00000011);
+  CHECK(flags2.test(my_flag::first));
+  CHECK(flags2.test(my_flag::second));
   CHECK(flags2.test(~test_flags_t(my_flag::third)));
+}
+
+TEST_CASE("enum_flags_with_custom_mask") {
+  using test_flags_t =
+      enum_flags<my_flag,
+                 make_enum_flags_mask_spec<my_flag>::from_enums_t<my_flag::first, my_flag::third>>;
+
+  STATIC_CHECK(std::is_trivially_copyable_v<test_flags_t>);
+  STATIC_CHECK(std::is_standard_layout_v<test_flags_t>);
+
+  STATIC_CHECK(to_bitset(test_flags_t::all()) == 0b00000101);
+  STATIC_CHECK(test_flags_t::all().has_any());
+  STATIC_CHECK(!test_flags_t::all().has_none());
+
+  STATIC_CHECK(to_bitset(test_flags_t::none()) == 0b00000000);
+  STATIC_CHECK(test_flags_t::none().has_none());
+  STATIC_CHECK(!test_flags_t::none().has_any());
+
+  test_flags_t flags1;
+  CHECK(to_bitset(flags1) == 0b00000000);
+  CHECK(!flags1.has_any());
+  CHECK(!static_cast<bool>(flags1));
+  CHECK(flags1.has_none());
+
+  test_flags_t flags2 = my_flag::first_and_second;
+  CHECK(to_bitset(flags2) == 0b00000001);
+  CHECK(flags2.has_any());
+  CHECK(static_cast<bool>(flags2));
+  CHECK(!flags2.has_none());
+  CHECK(flags2 & my_flag::first);
+  CHECK(!flags2.test(my_flag::second));
+  CHECK(!flags2.test(my_flag::third));
+  CHECK(!flags2.test(my_flag::first_and_second));
+
+  auto flags3 = test_flags_t() | my_flag::first | my_flag::third;
+  CHECK(to_bitset(flags3) == 0b00000101);
+  CHECK(flags3 & my_flag::first);
+  CHECK(!flags3.test(my_flag::second));
+  CHECK(flags3 & my_flag::third);
+  CHECK(!flags3.test(my_flag::first_and_second));
+
+  flags2 |= my_flag::third;
+  CHECK(to_bitset(flags2) == 0b00000101);
+  flags2 |= my_flag::second;
+  CHECK(to_bitset(flags2) == 0b00000101);
+  flags2 |= my_flag::first_and_second;
+  CHECK(to_bitset(flags2) == 0b00000101);
+
+  flags2 ^= my_flag::third;
+  CHECK(to_bitset(flags2) == 0b00000001);
+  flags2 ^= my_flag::second;
+  CHECK(to_bitset(flags2) == 0b00000001);
+  flags2 ^= my_flag::first_and_second;
+  CHECK(to_bitset(flags2) == 0b00000000);
+
+  flags3 &= my_flag::first_and_second;
+  CHECK(to_bitset(flags3) == 0b00000001);
+  flags3 &= my_flag::second;
+  CHECK(to_bitset(flags3) == 0b00000000);
+
+  flags1 = test_flags_t::all() & test_flags_t(my_flag::first_and_second);
+  CHECK(to_bitset(flags1) == 0b00000001);
+
+  flags1 = flags1 | my_flag::third | my_flag::second;
+  CHECK(to_bitset(flags1) == 0b00000101);
+
+  flags1 = flags1 ^ my_flag::first_and_second;
+  CHECK(to_bitset(flags1) == 0b00000100);
+
+  flags1.set(my_flag::second).reset(my_flag::third);
+  CHECK(to_bitset(flags1) == 0b00000000);
+
+  flags3.flip(my_flag::first_and_second);
+  CHECK(to_bitset(flags3) == 0b00000001);
+  flags3.flip(my_flag::first_and_second);
+  CHECK(to_bitset(flags3) == 0b00000000);
+
+  flags2.reset_all().flip(my_flag::first_and_second);
+  CHECK(to_bitset(flags2) == 0b00000001);
+  flags2.set_all().flip(my_flag::first_and_second);
+  CHECK(to_bitset(flags2) == 0b00000100);
+  flags2.flip_all();
+  CHECK(to_bitset(flags2) == 0b00000001);
+}
+
+TEST_CASE("enum_flags_deductions") {
+  STATIC_CHECK(std::is_same_v<decltype(enum_flags(my_flag::first)),
+                              enum_flags<my_flag, enum_flags_mask_unspecified_t>>);
+}
+
+TEST_CASE("enum_flags_underlying_type") {
+  // TBD
 }
 // NOLINTEND
